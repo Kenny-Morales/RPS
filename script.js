b48a84b8c01cc38217b0dfbe06e086c373b02ffb
Kenny Morales <kennymorales580@gmail.com>
2023-06-13 04:41:43 -0500

    made a switch for the random gen

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function compSelec() {
     var compNum = Math.floor(Math.random() * 3) + 1;
     console.log(compNum);
+    switch (compNum) {
+        case 1:
+            console.log("rock");
+            break;
+        case 2:
+            console.log("paper");
+            break;
+        case 3:
+            console.log("scissors");
+            break;
+        default:
+            return ("you broke it");
+    }
 }
+compSelec();
     
